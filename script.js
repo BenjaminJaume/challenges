6ab0b7ab327ad4f677786cb9b62ca1f57c345b55
BenjaminJaume <benjamin.jaume@gmail.com>
2019-08-17 19:12:38 +0200

    Final

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 const fs = require('fs');
 
-fs.readFile('./santa.txt', (err, data) => {
-  const directions = data.toString();
-  console.log(directions);
-});
+function question1() {
+  fs.readFile('./santa.txt', (err, data) => {
+    const directions = data.toString();
+    const directionsArray = directions.split('');
+    const answer = directionsArray.reduce((acc, currentValue) => {
+      if (currentValue === `(`) {
+        return (acc += 1);
+      } else if (currentValue === `)`) {
+        return (acc -= 1);
+      }
+    }, 0);
+    console.log(answer);
+  });
+}
+
+// question1();
+
+// 2 - When does Santa enters first the basement?
+
+function question2() {
+  fs.readFile('./santa.txt', (err, data) => {
+    const directions = data.toString();
+    const directionsArray = directions.split('');
+    let acc = 0;
+    let counter = 0;
+    const answer = directionsArray.some(currentValue => {
+      if (currentValue === `(`) {
+        acc += 1;
+      } else if (currentValue === `)`) {
+        acc -= 1;
+      }
+      counter++;
+      return acc < 0;
+    });
+    console.log('reached', counter);
+  });
+}
+
+question2();
